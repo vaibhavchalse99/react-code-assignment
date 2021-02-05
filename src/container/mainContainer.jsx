28d20576052ead4e56951bb719140d7fc5366324
vaibhavchalse99 <vaibhav.chalse99@gmail.com>
2021-02-05 19:25:31 +0530

    Add login logout buttons

--- src/container/mainContainer.jsx
+++ src/container/mainContainer.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import { Container } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 import MainComponent from "../component/mainComponent";
 import authContext from "./authContext";
 
 function MainContainer() {
   const [authenticated, setAuthenticated] = useState(false);
+  const history = useHistory();
+  const setState = () => {
+    setAuthenticated(false);
+  };
 
+  const handleClick = () => {
+    history.push("/account/signin");
+  };
+  const renderLoginButton = () => {
+    if (authenticated) {
+      return <button onClick={setState}>Logout</button>;
+    } else {
+      return <button onClick={handleClick}>Login</button>;
+    }
+  };
   return (
     <Container className="themed-container">
       <authContext.Provider value={{ authenticated, setAuthenticated }}>
-        <div> user is {`${authenticated ? "" : "not"} authenticated`} </div>
+        <div style={{ font: 50 }}>
+          user is {`${authenticated ? "" : "not"} authenticated`}{" "}
+        </div>
+        {renderLoginButton()}
         <MainComponent />
       </authContext.Provider>
     </Container>
