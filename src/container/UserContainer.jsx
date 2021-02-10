83452ac8c0672db564831b3eef1697ac96b6fd88
vaibhavchalse99 <vaibhav.chalse99@gmail.com>
2021-02-11 03:11:44 +0530

    Use 'useReducer' to handle local state of users

--- src/container/UserContainer.jsx
+++ src/container/UserContainer.jsx
@@ -1,11 +1,14 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
 import { Row } from "reactstrap";
 
 import UserComponent from "../component/UserComponent";
+import { userReducer, initialValue } from "../reducer/userReducer";
+import { GET_USERS } from "../helpers/stringHelper";
 
 const UserContainer = () => {
-  const [users, setUser] = useState([]);
+  const [usersState, dispatch] = useReducer(userReducer, initialValue);
+
   useEffect(() => {
     axios
       .get("https://reqres.in/api/users?page=2")
@@ -13,8 +16,11 @@ const UserContainer = () => {
         console.log(response);
         const { status, data } = response;
         if (status === 200) {
-          const { data: usersData } = data;
-          setUser(usersData);
+          const { data: users } = data;
+          dispatch({
+            type: GET_USERS,
+            payload: { users, error: "", loading: false },
+          });
         } else {
           throw new Error("Something went wrong");
         }
@@ -25,23 +31,29 @@ const UserContainer = () => {
   }, []);
 
   return (
-    <Fragment>
-      <h1>this is user component</h1>
-      <Row>
-        {users.map((ele) => {
-          const { id, email, first_name, last_name, avatar } = ele;
-          return (
-            <UserComponent
-              id={id}
-              email={email}
-              first_name={first_name}
-              last_name={last_name}
-              avatar={avatar}
-            />
-          );
-        })}
-      </Row>
-    </Fragment>
+    <>
+      {usersState.loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <>
+          <h1>this is user component</h1>
+          <Row>
+            {usersState.users.map((ele) => {
+              const { id, email, first_name, last_name, avatar } = ele;
+              return (
+                <UserComponent
+                  id={id}
+                  email={email}
+                  first_name={first_name}
+                  last_name={last_name}
+                  avatar={avatar}
+                />
+              );
+            })}
+          </Row>
+        </>
+      )}
+    </>
   );
 };
 
